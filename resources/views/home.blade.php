705a703abb412b23c4d125ac9bbd97507046e189
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2019-10-19 20:11:42 +0630

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="row">
+	<div class="col-12 pt-5">
+		<h4>Arkr Phyo</h4>
+	</div>
+</div>
+@endsection
+
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+       <h1>welcome home</h1>
+    </div>
+</div>
+@endsection
+
+
